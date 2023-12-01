08a90b129a1c4c2d40857b5554cc21cf0536b977
kali WSL <maximilianjentsch7@gmail.com>
2023-12-01 20:40:06 +0100

    TODO: fix multiple occurences

--- 1/day1.2.c
+++ 1/day1.2.c
@@ -4,99 +4,143 @@
 #include <ctype.h>
 #include <string.h>
 
-typedef struct number {
-    int number;
-    int position;
-} number;
-
 #define NOP 0
 
+typedef struct number {
+    char number;
+    int position;
+}
+number;
+
 int main() {
-  FILE *fp = fopen("input.txt", "r");
-  char *line = NULL;
-  size_t len = 0;
-  ssize_t read;
+    FILE * fp = fopen("tinput.txt", "r");
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
 
-  (fp == NULL) ? exit(EXIT_FAILURE) : NOP;
+    (fp == NULL) ? exit(EXIT_FAILURE): NOP;
 
+    int result = 0;
 
-  int result = 0;
+    const char * errstr;
 
-  const char *errstr;
-  char firstNumber = NULL;
-  char lastNumber = NULL;
+    int lines = 0;
 
-  int lines = 0;
+    char words[10][10] = {
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine"
+    };
 
-  char words[10][10] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    while ((read = getline( & line, & len, fp)) != -1) {
+        lines++;
 
-  while ((read = getline(&line, &len, fp)) != -1) {
+        //printf("Processing line %d: %s", lines, line);
 
-    lines++;
+        number* fnumbers = malloc(sizeof(number) * 100);
+        if (fnumbers == NULL) {
+            // Handle memory allocation failure
+            fprintf(stderr, "Memory allocation failed\n");
+            exit(EXIT_FAILURE);
+        }
 
-    number *fnumbers = malloc(sizeof(number)*100);
-    int pnumber = 0;
+        //printf("Before searching for characters\n");
+        int pnumber = 0;
+        //find all occurences of 0..9 oder zero..nine in the line and record them in a number struct
+        for (int x = 48; x <= 57; x++) {
+            char * e;
+            int index;
 
-            //find all occurences of 0..9 oder zero..nine in the line and record them in a number struct
-            for(int x = 48; x < 57; x++){
-                char *e;
-                int index;
-
-                e = strchr(line, x);
-                index = (int)(e - line);
-
-                if(index < 0 || index > len){
-                    //printf("not found!\n");
-                    continue;
-                }
-
-                number j;
-		j.number = x - 48;
-		j.position = index;
-                fnumbers[pnumber] = j;
-                pnumber++;
-                printf("%s", line);
-                printf("found char %c (%d)(%d) at position %d in line %d!\n", (char)x, x, x- 48, index, lines);
+            e = strchr(line, x);
+            if(e == NULL){
+                continue;
             }
 
-            for(int j = 0; j < 10; j++){
-                char *e;
-                int index;
+            index = (int)(e - line);
 
-                e = strstr(line, words[j]);
-                index = (int)(e - line);
-
-                if(index < 0 || index > len){
-                    //printf("not found!\n");
-                    continue;
-		}
-
-                number x;
-		x.number = j;
-		x.position = index;
-                fnumbers[pnumber] = x;
-                pnumber++;
-                printf("%s", line);
-                printf("found string %s (%d) at position %d in line %d!\n", words[j], j, index, lines);
+            if (index < 0 || index > len) {
+                // printf("not found!\n");
+                continue;
             }
 
-    //find the number with the min and max position and assign them to first and last number
+            number j;
+            j.number = x;
+            j.position = index;
+            fnumbers[pnumber] = j;
+            pnumber++;
+            //printf("found char %c (%d)(%d) at position %d in line %d!\n", (char) x, x, x - 48, index, lines);
+        }
 
+        //printf("Before searching for strings\n");
+        for (int j = 0; j < 10; j++) {
+            char * e;
+            int index;
 
-    //proceed as expected
-    /*char endNum[5] = {firstNumber, lastNumber};
+            e = strstr(line, words[j]);
+            if(e == NULL){
+                continue;
+            }
 
-    printf("line %d: %s", lines, line);
-    printf("number in line %d: %c & ", lines, firstNumber);
-    printf("%c\t", lastNumber);
-    printf("endnum: %s\n", endNum);
+            index = (int)(e - line);
 
-    result += atoi(endNum);*/
+            if (index < 0 || index > len) {
+                //printf("not found!\n");
+                continue;
+            }
 
-    line = NULL;
-    free(fnumbers);
-  }
+            number x;
+            x.number = j + 48;
+            x.position = index;
+            fnumbers[pnumber] = x;
+            pnumber++;
+            //printf("found string %s (%d) at position %d in line %d! Equivalent to: %c!\n", words[j], j, index, lines, j + 48);
+        }
 
-  //printf("result: %d\n", result);
+        //printf("Before finding lowest and highest numbers\n");
+        //find the number with the min and max position and assign them to first and last number
+        int lowestPos = 100;
+        number * lowestNumber = NULL;
+        int highestPos = -1;
+        number * highestNumber = NULL;
+
+        for (int k = 0; k < pnumber; k++) {
+            //printf("k: %d\n", k);
+            int currentPos = fnumbers[k].position;
+
+            if (currentPos < lowestPos) {
+                //printf("pos %d is lower than current %d!\n", currentPos, lowestPos);
+                lowestPos = currentPos;
+                lowestNumber = &fnumbers[k];
+            }
+            if (currentPos > highestPos) {
+                //printf("pos %d is higher than current %d!\n", currentPos, highestPos);
+                highestPos = currentPos;
+                highestNumber = &fnumbers[k];
+            }
+        }
+
+        //proceed as expected
+        char endNum[5] = {lowestNumber->number, highestNumber->number, '\0'};
+        //printf("lowest Pos: %d\n", lowestPos);
+        //printf("highest Pos: %d\n", highestPos);
+        printf("line %d: %s", lines, line);
+        printf("number in line %d: %c (%d) & ", lines, lowestNumber->number, (int)lowestNumber->number);
+        printf("%c (%d)\n", highestNumber->number, (int)highestNumber->number);
+        printf("endnum: %s\n------\n\n", endNum);
+
+        result += atoi(endNum);
+
+        line = NULL;
+        free(fnumbers);
+    }
+
+    printf("result: %d\n", result);
 
 }
